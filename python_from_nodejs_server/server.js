1a621b99c28dab7b63e778edd75eb981d875b280
JosephOHagan <josephaohagan@gmail.com>
2016-12-06 12:33:32 +0000

    Python from Nodejs example

---
+++ python_from_nodejs_server/server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+// app.use('/img', express.static(__dirname + '/img'));
+
+app.get('/', function(req, res) {
+	res.sendFile('views/demo.html', { root: __dirname });
+});
+
+var port = process.env.PORT||3000;
+http.listen(port, function() {
+	console.log('listening on *:3000');
+});
+
+
+/*
+	Python and JavaScript talking to one another
+	Possible method 1: https://github.com/extrabacon/python-shell
+*/
+
+var PythonShell = require('python-shell');
+
+PythonShell.run('my_script.py', function (err) {
+  if (err) throw err;
+  console.log('finished');
+});
+
+
+/*
+	Python and JavaScript talking to one another
+	Possible method 2: http://www.sohamkamani.com/blog/2015/08/21/python-nodejs-comm/
+*/
+	
+var spawn = require('child_process').spawn,
+	py = spawn('python', ['compute_input.py']),
+    data = [1,2,3,4,5,6,7,8,9],
+    dataString = '';
+
+py.stdout.on('data', function(data){
+  	dataString += data.toString(); 
+});
+
+py.stdout.on('end', function(){
+	console.log('Sum of numbers = HERE =',dataString);
+});
+
+py.stdin.write(JSON.stringify(data));
+py.stdin.end();
---
+++ python_from_nodejs_server/server_implementation.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var parse = require('csv-parse');
+var fs = require('fs');
+var pythonShell = require('python-shell');
+
+// app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+// app.use('/img', express.static(__dirname + '/img'));
+
+app.get('/', function(req, res) {
+    res.sendFile('views/demo.html', { root: __dirname });
+});
+
+
+// API ports
+app.get('/recs/:id', function(req, res) {
+
+    var id;
+    if   (!req.params.id) id = 0;
+    else id = req.params.id;
+
+    // read two files
+    fs.readFile('./recommendations.csv', 'utf8', function(err1, recommendationsFile) {
+        fs.readFile('./Restaurant.csv', 'utf8', function(err2, restaurantsFile) {
+            // parse the files
+            parse(recommendationsFile, {}, function(err3, recommendations) {
+                parse(restaurantsFile, {columns: true}, function(err4, restaurants) {
+                    // filter out the relevant recommendations
+                    res.send(recommendations.filter(function(recommendation) {
+                        return recommendation[0] == id;
+                    }).map(function(recommendation) {
+                        // find the information about each restaurant
+                        return restaurants.find(function(restaurant) {
+                            return restaurant['RestaurantId'] ==
+                                recommendation[1];
+                        });
+                    }));
+                });
+            });
+        });
+    });
+});
+
+/* Call Python script to generate recommendation CSV automatically */
+pythonShell.run('script.py', function (err) {
+	if (err) throw err;
+	console.log('Recommendation script success');
+});
+
+var port = process.env.PORT||3000;
+http.listen(port, function() {
+    console.log('listening on *:3000');
+});
